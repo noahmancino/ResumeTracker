617a09193e026338d68221d6e014b063e0472506
noahmancino <noahdanielmancino@gmail.com>
2021-09-12 15:35:57 -0400

    form

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, redirect, url_for
-from flask_bootstrap import Boostrap
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config.ini")
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = config["APP"]["SECRET_KEY"]
+print(config["APP"]["SECRET_KEY"])
 
 
 @app.route('/')
