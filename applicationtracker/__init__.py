7740c0dfc60f371a276275f35713cc38d430524d
noahmancino <noahdanielmancino@gmail.com>
2021-09-22 18:02:00 -0400

    deploying

--- applicationtracker/__init__.py
+++ applicationtracker/__init__.py
@@ -3,13 +3,16 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 import configparser
 from flask_bcrypt import Bcrypt
+import os
 
+'''
 config = configparser.ConfigParser()
 config.read("config.ini")
+'''
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = config["APP"]["SECRET_KEY"]
-app.config["SQLALCHEMY_DATABASE_URI"] = config["DATABASE"]["URI"]
+app.config["SECRET_KEY"] = os.getenv('APP_SECRET_KEY', 'default')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('SQLITE_DATABASE_URI', "sqlite:///site.db")
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
