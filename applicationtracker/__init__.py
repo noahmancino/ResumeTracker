e247783c6b8ce5238035840eab875ea42f36c4b5
noahmancino <noahdanielmancino@gmail.com>
2021-09-22 18:29:32 -0400

    deploying

--- applicationtracker/__init__.py
+++ applicationtracker/__init__.py
@@ -12,7 +12,7 @@ config.read("config.ini")
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv('APP_SECRET_KEY', 'default')
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('SQLITE_DATABASE_URI', "sqlite:///site.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URI', "sqlite:///site.db")
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
