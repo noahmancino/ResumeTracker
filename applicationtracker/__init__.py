6748d7f091e20248cd7ba66c84efa22f43202443
noahmancino <noahdanielmancino@gmail.com>
2021-09-22 18:44:21 -0400

    deploying

--- applicationtracker/__init__.py
+++ applicationtracker/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-import configparser
+import psycopg2
 from flask_bcrypt import Bcrypt
 import os
 
